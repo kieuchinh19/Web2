36432afeb922331feda042fd51d6badc62269cb3
kieuchinh19 <kieuchinh237@gmail.com>
2026-06-03 18:30:03 +0700

    First commit

---
+++ src/test/java/com/web2/chinh/ChinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.chinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
